db36d61fc5946ea49a3b1d8ee4f0bb0b91599f4a
shishir <sk3432@drexel.edu>
2016-04-06 18:09:38 -0400

    test cases for select method of SGP complete

--- src/test/scala/edu/drexel/cs/dbgroup/temporalgraph/representations/SnapshotGraphParallelSuite.scala
+++ src/test/scala/edu/drexel/cs/dbgroup/temporalgraph/representations/SnapshotGraphParallelSuite.scala
@@ -28,13 +28,7 @@ class SnapshotGraphParallelSuite  extends FunSuite with BeforeAndAfter {
     }
   }
 
-  test("select function - the given inverval is present in the graph"){
-    var intvs: Seq[Interval] = Seq[Interval]()
-    var gps: ParSeq[Graph[String, Int]] = ParSeq[Graph[String, Int]]()
-
-    //creating the first graph
-    val testInterval1 = Interval(LocalDate.parse("2014-01-01"), LocalDate.parse("2015-01-01"))
-    intvs = intvs :+ testInterval1
+  def makeGraph1(): Graph[String, Int] = {
     var users: RDD[(VertexId, String)] = ProgramContext.sc.parallelize(Array[(VertexId, String)](
       (1L, "John"),
       (2L, "Mike"),
@@ -45,48 +39,170 @@ class SnapshotGraphParallelSuite  extends FunSuite with BeforeAndAfter {
       Edge(2L, 3L, 32),
       Edge(1L, 3L, 21)
     )))
-    val graph1 = Graph(users, edges, "Default", edgeStorageLevel = StorageLevel.MEMORY_ONLY_SER, vertexStorageLevel = StorageLevel.MEMORY_ONLY_SER)
-    gps = gps :+ graph1
+    Graph(users, edges, "Default", edgeStorageLevel = StorageLevel.MEMORY_ONLY_SER, vertexStorageLevel = StorageLevel.MEMORY_ONLY_SER)
+  }
 
-    //creating the second graph
-    val testInterval2 = Interval(LocalDate.parse("2015-01-01"), LocalDate.parse("2016-01-01"))
-    intvs = intvs :+ testInterval2
-    users = ProgramContext.sc.parallelize(Array[(VertexId, String)](
+  def makeGraph2(): Graph[String, Int] = {
+    var users = ProgramContext.sc.parallelize(Array[(VertexId, String)](
       (4L, "Julia"),
       (5L, "Vera"),
       (6L, "Hamlima"),
       (1L, "John")
     ))
-    edges = EdgeRDD.fromEdges(ProgramContext.sc.parallelize(Array(
+    var edges : EdgeRDD[Int] = EdgeRDD.fromEdges(ProgramContext.sc.parallelize(Array(
       Edge(4L, 5L, 12),
       Edge(1L, 4L, 102),
       Edge(1L, 5L, 5)
     )))
-    val graph2 = Graph(users, edges, "Default", edgeStorageLevel = StorageLevel.MEMORY_ONLY_SER, vertexStorageLevel = StorageLevel.MEMORY_ONLY_SER)
-    gps = gps :+ graph2
-
-    val sgp1 = new SnapshotGraphParallel(intvs, gps)
-
-    val intervalExpected = testInterval2
-    val expectedSGP = new SnapshotGraphParallel(Seq[Interval](intervalExpected), ParSeq[Graph[String, Int]](graph2))
-
-    //casting graph to SnapshotGraphParallel
-    val testSGP = sgp1.select(testInterval2) match {
-      case e: SnapshotGraphParallel[String, Int] => e
-      case _ => null
-    }
-
-    assert(expectedSGP === testSGP)
+    Graph(users, edges, "Default", edgeStorageLevel = StorageLevel.MEMORY_ONLY_SER, vertexStorageLevel = StorageLevel.MEMORY_ONLY_SER)
   }
 
-  test("select function - the given inverval is inside the graph interaval but the graph does not have that interval")(pending)
+  def makeGraph3(): Graph[String, Int] = {
+    var users = ProgramContext.sc.parallelize(Array[(VertexId, String)](
+      (7L, "Someone"),
+      (8L, "Noone"),
+      (9L, "Somebody"),
+      (4L, "Julia")
+    ))
+    var edges : EdgeRDD[Int] = EdgeRDD.fromEdges(ProgramContext.sc.parallelize(Array(
+      Edge(7L, 8L, 22),
+      Edge(8L, 9L, 72),
+      Edge(4L, 7L, 5),
+      Edge(4L, 8L, 66),
+      Edge(4L, 9L, 76)
+    )))
+    Graph(users, edges, "Default", edgeStorageLevel = StorageLevel.MEMORY_ONLY_SER, vertexStorageLevel = StorageLevel.MEMORY_ONLY_SER)
+  }
 
-  test("select function - the given inverval is outside the graph interaval")(pending)
+  test("select function - the given inverval is present"){
+    var intervals: Seq[Interval] = Seq[Interval]()
+    var graphs: ParSeq[Graph[String, Int]] = ParSeq[Graph[String, Int]]()
 
-  test("select function - the start of given inverval is inside but end is outside the graph interaval")(pending)
+    //creating the first graph
+    val testInterval1 = Interval(LocalDate.parse("2014-01-01"), LocalDate.parse("2015-01-01"))
+    val testInterval2 = Interval(LocalDate.parse("2015-01-01"), LocalDate.parse("2016-01-01"))
+    val testInterval3 = Interval(LocalDate.parse("2016-01-01"), LocalDate.parse("2017-01-01"))
 
-  test("select function - the graph has only one time interval")(pending)
+    intervals = intervals :+ testInterval1 :+ testInterval2 :+ testInterval3
+    graphs = graphs :+ makeGraph1() :+ makeGraph2() :+ makeGraph3()
 
-  test("select function - the graph is empty")(pending)
+    val sgp = new SnapshotGraphParallel(intervals, graphs)
+    val actualSGP = sgp.select(testInterval2)
+    val expectedSGP = new SnapshotGraphParallel(Seq[Interval](testInterval2), ParSeq[Graph[String, Int]](makeGraph2()))
+
+    assert(expectedSGP.vertices.collect() === actualSGP.vertices.collect())
+    assert(expectedSGP.edges.collect() === actualSGP.edges.collect())
+  }
+
+  test("select function - the given inverval is present but is non-matching"){
+    var intervals: Seq[Interval] = Seq[Interval]()
+    var graphs: ParSeq[Graph[String, Int]] = ParSeq[Graph[String, Int]]()
+
+    //creating the first graph
+    val testInterval1 = Interval(LocalDate.parse("2014-01-01"), LocalDate.parse("2015-01-01"))
+    val testInterval2 = Interval(LocalDate.parse("2015-01-01"), LocalDate.parse("2016-01-01"))
+    val testInterval3 = Interval(LocalDate.parse("2016-01-01"), LocalDate.parse("2017-01-01"))
+
+    intervals = intervals :+ testInterval1 :+ testInterval2 :+ testInterval3
+    graphs = graphs :+ makeGraph1() :+ makeGraph2() :+ makeGraph3()
+
+    val sgp = new SnapshotGraphParallel(intervals, graphs)
+    val actualSGP = sgp.select(Interval(LocalDate.parse("2015-01-01"), LocalDate.parse("2017-01-01")))
+
+    val expectedInterval =  Seq[Interval]() :+ testInterval2 :+ testInterval3
+    val expectedGraph : ParSeq[Graph[String, Int]] = ParSeq[Graph[String, Int]]() :+ makeGraph2() :+ makeGraph3()
+    val expectedSGP = new SnapshotGraphParallel(expectedInterval, expectedGraph)
+
+    assert(expectedSGP.vertices.collect() === actualSGP.vertices.collect())
+    assert(expectedSGP.edges.collect() === actualSGP.edges.collect())
+  }
+
+  test("select function - the given inverval is outside"){
+    var intervals: Seq[Interval] = Seq[Interval]()
+    var graphs: ParSeq[Graph[String, Int]] = ParSeq[Graph[String, Int]]()
+
+    //creating the first graph
+    val testInterval1 = Interval(LocalDate.parse("2014-01-01"), LocalDate.parse("2015-01-01"))
+    val testInterval2 = Interval(LocalDate.parse("2015-01-01"), LocalDate.parse("2016-01-01"))
+    val testInterval3 = Interval(LocalDate.parse("2016-01-01"), LocalDate.parse("2017-01-01"))
+
+    intervals = intervals :+ testInterval1 :+ testInterval2 :+ testInterval3
+    graphs = graphs :+ makeGraph1() :+ makeGraph2() :+ makeGraph3()
+
+    val sgp = new SnapshotGraphParallel(intervals, graphs)
+    val actualSGP1 = sgp.select(Interval(LocalDate.parse("2018-01-01"), LocalDate.parse("2019-01-01")))
+    val actualSGP2 = sgp.select(Interval(LocalDate.parse("2013-01-01"), LocalDate.parse("2014-01-01")))
+    val actualSGP3 = sgp.select(Interval(LocalDate.parse("2019-01-01"), LocalDate.parse("2021-01-01")))
+
+    val expectedSGP =  SnapshotGraphParallel.emptyGraph[String, Int]()
+
+    assert(expectedSGP.vertices.collect() === actualSGP1.vertices.collect())
+    assert(expectedSGP.edges.collect() === actualSGP1.edges.collect())
+    assert(expectedSGP.edges.collect() === actualSGP2.edges.collect())
+    assert(expectedSGP.edges.collect() === actualSGP3.edges.collect())
+  }
+
+  test("select function - the given inverval partially overlaps (start is outside the bounds)"){
+    val testInterval1 = Interval(LocalDate.parse("2014-01-01"), LocalDate.parse("2015-01-01"))
+    val testInterval2 = Interval(LocalDate.parse("2015-01-01"), LocalDate.parse("2016-01-01"))
+    val testInterval3 = Interval(LocalDate.parse("2016-01-01"), LocalDate.parse("2017-01-01"))
+
+    val intervals: Seq[Interval] = Seq[Interval](testInterval1, testInterval2, testInterval3)
+    val graphs: ParSeq[Graph[String, Int]] = ParSeq[Graph[String, Int]](makeGraph1(), makeGraph2(), makeGraph3())
+
+    val sgp = new SnapshotGraphParallel(intervals, graphs)
+    val actualSGP = sgp.select(Interval(LocalDate.parse("2011-01-01"), LocalDate.parse("2016-01-01")))
+
+    val expectedInterval =  Seq[Interval]() :+ testInterval1 :+ testInterval2
+    val expectedGraph : ParSeq[Graph[String, Int]] = ParSeq[Graph[String, Int]]() :+ makeGraph1() :+ makeGraph2()
+    val expectedSGP = new SnapshotGraphParallel(expectedInterval, expectedGraph)
+
+    assert(expectedSGP.vertices.collect() === actualSGP.vertices.collect())
+    assert(expectedSGP.edges.collect() === actualSGP.edges.collect())
+  }
+
+  test("select function - the given inverval partially overlaps (end is outside the bounds)"){
+    val testInterval1 = Interval(LocalDate.parse("2014-01-01"), LocalDate.parse("2015-01-01"))
+    val testInterval2 = Interval(LocalDate.parse("2015-01-01"), LocalDate.parse("2016-01-01"))
+    val testInterval3 = Interval(LocalDate.parse("2016-01-01"), LocalDate.parse("2017-01-01"))
+
+    val intervals: Seq[Interval] = Seq[Interval](testInterval1, testInterval2, testInterval3)
+    val graphs: ParSeq[Graph[String, Int]] = ParSeq[Graph[String, Int]](makeGraph1(), makeGraph2(), makeGraph3())
+
+    val sgp = new SnapshotGraphParallel(intervals, graphs)
+    val actualSGP = sgp.select(Interval(LocalDate.parse("2015-01-01"), LocalDate.parse("2019-01-01")))
+
+    val expectedInterval =  Seq[Interval]() :+ testInterval2 :+ testInterval3
+    val expectedGraph : ParSeq[Graph[String, Int]] = ParSeq[Graph[String, Int]](makeGraph2(), makeGraph3())
+    val expectedSGP = new SnapshotGraphParallel(expectedInterval, expectedGraph)
+
+    assert(expectedSGP.vertices.collect() === actualSGP.vertices.collect())
+    assert(expectedSGP.edges.collect() === actualSGP.edges.collect())
+  }
+
+  test("select function - the given interval is equal to the graph's interval"){
+    val testInterval1 = Interval(LocalDate.parse("2014-01-01"), LocalDate.parse("2015-01-01"))
+    val testInterval2 = Interval(LocalDate.parse("2015-01-01"), LocalDate.parse("2016-01-01"))
+    val testInterval3 = Interval(LocalDate.parse("2016-01-01"), LocalDate.parse("2017-01-01"))
+
+    val intervals: Seq[Interval] = Seq[Interval](testInterval1, testInterval2, testInterval3)
+    val graphs: ParSeq[Graph[String, Int]] = ParSeq[Graph[String, Int]](makeGraph1(), makeGraph2(), makeGraph3())
+
+    val sgp = new SnapshotGraphParallel(intervals, graphs)
+    val actualSGP = sgp.select(Interval(LocalDate.parse("2014-01-01"), LocalDate.parse("2017-01-01")))
+    val expectedSGP = sgp
+
+    assert(expectedSGP.vertices.collect() === actualSGP.vertices.collect())
+    assert(expectedSGP.edges.collect() === actualSGP.edges.collect())
+  }
+
+  test("select function - the graph is empty"){
+    val sgp = new SnapshotGraphParallel(Seq[Interval](), ParSeq[Graph[String, Int]]())
+    val actualSGP = sgp.select(Interval(LocalDate.parse("2014-01-01"), LocalDate.parse("2017-01-01")))
+    val expectedSGP = sgp
+
+    assert(expectedSGP.vertices.collect() === actualSGP.vertices.collect())
+    assert(expectedSGP.edges.collect() === actualSGP.edges.collect())
+  }
 
 }
