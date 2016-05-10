0411b3b52dcd5e39c9d0343515d771c4dd0944ee
Vera Moffitt <zaychik@drexel.edu>
2016-05-10 14:31:37 -0400

    These versions are incomplete and preliminary. Checked in only so that GraphLoader compiles.

--- src/main/scala/edu/drexel/cs/dbgroup/temporalgraph/representations/SnapshotGraphWithSchema.scala
+++ src/main/scala/edu/drexel/cs/dbgroup/temporalgraph/representations/SnapshotGraphWithSchema.scala
@@ -1,159 +1,166 @@
 package edu.drexel.cs.dbgroup.temporalgraph.representations
 
 import scala.collection.parallel.ParSeq
-import scala.collection.mutable.Buffer
 import scala.reflect.ClassTag
 import scala.util.control._
 import scala.language.implicitConversions
 import java.time.LocalDate
+import java.sql.Date
 
 import org.apache.hadoop.conf._
 import org.apache.hadoop.fs._
 
 import org.apache.spark.SparkContext
 import org.apache.spark.Partition
-
 import org.apache.spark.graphx._
 import org.apache.spark.rdd._
 import org.apache.spark.storage.{RDDBlockId,StorageLevel}
-import org.apache.spark.sql.catalyst.InternalRow
-import org.apache.spark.sql.catalyst.expressions.{GenericInternalRow,NamedExpression,InterpretedProjection}
-import org.apache.spark.sql.types.StructField
+import org.apache.spark.storage.StorageLevel._
+import org.apache.spark.sql.functions.udf
+import org.apache.spark.sql.{DataFrame,Row,Column}
+import org.apache.spark.sql.types.{StructType,StructField}
 
 import edu.drexel.cs.dbgroup.temporalgraph._
-import edu.drexel.cs.dbgroup.temporalgraph.util.{MultifileLoad,GraphLoader,TypeParser}
+import edu.drexel.cs.dbgroup.temporalgraph.util.TempGraphOps._
 
-class SnapshotGraphWithSchema(intvs: Seq[Interval], gps: ParSeq[Graph[InternalRow,InternalRow]]) extends SnapshotGraphParallel[InternalRow,InternalRow](intvs, gps) with TemporalGraphWithSchema with Serializable {
-  private var schema: GraphSpec = GraphSpec(Seq(), Seq())
+class SnapshotGraphWithSchema(intvs: Seq[Interval], verts: DataFrame, edgs: DataFrame, gps: ParSeq[Graph[Row,Row]], storLevel: StorageLevel = StorageLevel.MEMORY_ONLY) extends TGraphWithSchema(intvs, verts, edgs, storLevel) {
+  import sqlContext.implicits._
 
-  override def getSchema(): GraphSpec = schema
-  def setSchema(sch: GraphSpec): Unit = { schema = sch }
+  protected val graphs: ParSeq[Graph[Row, Row]] = gps
 
-  private def parentWrapper(g: SnapshotGraphParallel[InternalRow,InternalRow], newSchema: GraphSpec): SnapshotGraphWithSchema = {
-    val res = new SnapshotGraphWithSchema(g.intervals, g.graphs)
-    res.setSchema(newSchema)
-    res
+  override def materialize() = {
+    allVertices.count
+    allEdges.count
+    graphs.foreach { x =>
+      if (!x.edges.isEmpty)
+        x.numEdges
+      if (!x.vertices.isEmpty)
+        x.numVertices
+    }
   }
 
-  override def mapVerticesWIndex(map: (VertexId, TimeIndex, InternalRow) => InternalRow, newSchema: GraphSpec): SnapshotGraphWithSchema = parentWrapper(super.mapVerticesWIndex(map), newSchema)
-  override def mapEdgesWIndex(map: (Edge[InternalRow], TimeIndex) => InternalRow, newSchema: GraphSpec): SnapshotGraphWithSchema = parentWrapper(super.mapEdgesWIndex(map), newSchema)
-  override def aggregate(res: Resolution, vsem: AggregateSemantics.Value, esem: AggregateSemantics.Value, vAggFunc: (InternalRow, InternalRow) => InternalRow, eAggFunc: (InternalRow, InternalRow) => InternalRow): SnapshotGraphWithSchema = parentWrapper(super.aggregate(res, vsem, esem, vAggFunc, eAggFunc), schema)
-
-  //we can support filtering (i.e. keeping only certain columsn)
-  //and aliasing
-  override def projectVertices(input: Seq[NamedExpression]): TemporalGraphWithSchema = {
-    //TODO: check whether this projection actually changes anything
-    //before going through the effort
-    val projection = new InterpretedProjection(input, schema.vertexSchemaAsAttributes)
-
-    val res = new SnapshotGraphWithSchema(intervals, graphs.map { g =>
-      g.mapVertices( (id: VertexId, attr: InternalRow) => projection(attr))
-    })
-    res.setSchema(GraphSpec(input.map(_.toAttribute).map(a => StructField(a.name, a.dataType,a.nullable, a.metadata)), schema.getEdgeSchema))
-    res
+  override def getSnapshot(time: LocalDate): Graph[Row,Row] = {
+    val index = intervals.indexWhere(a => a.contains(time))
+    if (index >= 0) {
+      graphs(index)
+    } else
+      Graph[Row,Row](ProgramContext.sc.emptyRDD, ProgramContext.sc.emptyRDD)
   }
 
-  override def projectEdges(input: Seq[NamedExpression]): TemporalGraphWithSchema = {
-    //TODO: check whether this projection actually changes anything
-    //before going through the effort
-    val projection = new InterpretedProjection(input, schema.edgeSchemaAsAttributes)
+  /** Query operations */
 
-    val res = new SnapshotGraphWithSchema(intervals, graphs.map { g =>
-      g.mapEdges{ e: Edge[InternalRow] => projection(e.attr)}
-    })
-    res.setSchema(GraphSpec(schema.getVertexSchema, input.map(_.toAttribute).map(a => StructField(a.name, a.dataType,a.nullable, a.metadata))))
-    res
+  override def slice(bound: Interval): SnapshotGraphWithSchema = {
+    if (span.start.isEqual(bound.start) && span.end.isEqual(bound.end)) return this
+
+    if (!span.intersects(bound)) {
+      return SnapshotGraphWithSchema.emptyGraph(schema)
+    }
+
+    val startBound = if (bound.start.isAfter(span.start)) bound.start else span.start
+    val endBound = if (bound.end.isBefore(span.end)) bound.end else span.end
+    val selectBound:Interval = Interval(startBound, endBound)
+    //compute indices of start and stop
+    val selectStart:Int = intervals.indexWhere(intv => intv.intersects(selectBound))
+    var selectStop:Int = intervals.lastIndexWhere(intv => intv.intersects(selectBound))
+    if (selectStop < 0) selectStop = intervals.size
+    val newIntvs: Seq[Interval] = intervals.slice(selectStart, selectStop)
+
+    val sb = Date.valueOf(startBound)
+    val eb = Date.valueOf(endBound)
+    val mnf: (Date => Date) = (in: Date) => if (in.before(eb)) in else eb
+    val mxf: (Date => Date) = (in: Date) => if (sb.before(in)) in else sb
+    val maxFunc = udf(mxf)
+    val minFunc = udf(mnf)
+    val clsv = Array(allVertices("vid"), maxFunc($"estart").as("estart"), minFunc($"eend").as("eend")) ++ allVertices.columns.drop(3).map(c => allVertices(c))
+    val clse = Array(allEdges("vid1"), allEdges("vid2"), maxFunc($"estart").as("estart"), minFunc($"eend").as("eend")) ++ allEdges.columns.drop(4).map(c => allEdges(c))
+
+    new SnapshotGraphWithSchema(newIntvs, allVertices.where(!($"estart" >= eb || $"eend" <= sb)).select(clsv: _*), 
+      allEdges.where(!($"estart" >= eb || $"eend" <= sb)).select(clse: _*), 
+      graphs.slice(selectStart, selectStop), storageLevel)
+
   }
+
+  override def union(other: TGraph[Row, Row], vFunc: (Row, Row) => Row, eFunc: (Row, Row) => Row): SnapshotGraphWithSchema = {
+    throw new UnsupportedOperationException("not yet implemented")
+  }
+
+  override def intersection(other: TGraph[Row, Row], vFunc: (Row, Row) => Row, eFunc: (Row, Row) => Row): SnapshotGraphWithSchema = {
+    throw new UnsupportedOperationException("not yet implemented")
+  }
+
+  override def pregel[A: ClassTag]
+     (initialMsg: A, defValue: A, maxIterations: Int = Int.MaxValue,
+       activeDirection: EdgeDirection = EdgeDirection.Either)
+     (vprog: (VertexId, Row, A) => Row,
+       sendMsg: EdgeTriplet[Row, Row] => Iterator[(VertexId, A)],
+       mergeMsg: (A, A) => A): SnapshotGraphWithSchema = {
+    throw new UnsupportedOperationException("not yet implemented")
+  }
+
+  override def degree: RDD[(VertexId, (Interval, Int))] = {
+    throw new UnsupportedOperationException("not yet implemented")
+  }
+
+  /** Spark-specific */
+
+  override def numPartitions(): Int = {
+    graphs.filterNot(_.edges.isEmpty).map(_.edges.partitions.size).reduce(_ + _)
+  }
+
+  override def persist(newLevel: StorageLevel = MEMORY_ONLY): SnapshotGraphWithSchema = {
+    super.persist(newLevel)
+    //persist each graph
+    //this will throw an exception if the graphs are already persisted
+    //with a different storage level
+    graphs.map(g => g.persist(newLevel))
+    this
+  }
+
+  override def unpersist(blocking: Boolean = true): SnapshotGraphWithSchema = {
+    super.unpersist(blocking)
+    graphs.map(_.unpersist(blocking))
+    this
+  }
+
+  override def partitionBy(pst: PartitionStrategyType.Value, runs: Int): SnapshotGraphWithSchema = {
+    partitionBy(pst, runs, 0)
+  }
+
+  override def partitionBy(pst: PartitionStrategyType.Value, runs: Int, parts: Int): SnapshotGraphWithSchema = {
+    if (pst != PartitionStrategyType.None) {
+      //not changing the intervals, only the graphs at their indices
+      //each partition strategy for SG needs information about the graph
+
+      //use that strategy to partition each of the snapshots
+      new SnapshotGraphWithSchema(intervals, allVertices, allEdges, graphs.zipWithIndex.map { case (g,i) =>
+        val numParts: Int = if (parts > 0) parts else g.edges.partitions.size
+        g.partitionBy(PartitionStrategies.makeStrategy(pst, i, graphs.size, runs), numParts)
+      }, storageLevel)
+    } else
+      this
+  }
+
+  override protected def fromDataFrames(vs: DataFrame, es: DataFrame, storLevel: StorageLevel = StorageLevel.MEMORY_ONLY): SnapshotGraphWithSchema = SnapshotGraphWithSchema.fromDataFrames(vs, es, storLevel)
+  override protected def emptyGraph(sch: GraphSpec) = SnapshotGraphWithSchema.emptyGraph(sch)
 }
 
 object SnapshotGraphWithSchema {
-  final def loadData(dataPath: String, start: LocalDate, end: LocalDate): SnapshotGraphWithSchema = {
-    loadWithSchema(dataPath, start, end, GraphLoader.loadGraphDescription(dataPath))
+  private val sqlContext = ProgramContext.getSqlContext
+  import sqlContext.implicits._
+
+  def fromDataFrames(vs: DataFrame, es: DataFrame, storLevel: StorageLevel = StorageLevel.MEMORY_ONLY): SnapshotGraphWithSchema = {
+    val intervals = TGraphWithSchema.computeIntervals(vs, es)
+
+    val graphs: ParSeq[Graph[Row,Row]] = intervals.map{ p =>
+      val ps = Date.valueOf(p.start)
+      val pe = Date.valueOf(p.end)
+      Graph(vs.where(!($"estart" >= pe) || $"eend" <= ps).map(row => (row.getLong(0), row)), 
+        es.where(!($"estart" >= pe || $"eend" <= ps)).map(row => Edge(row.getLong(0), row.getLong(1), row)), Row(), storLevel)
+    }.par
+
+    new SnapshotGraphWithSchema(intervals, vs, es, graphs, storLevel)
+
   }
 
-  final def loadWithSchema(dataPath: String, start: LocalDate, end:LocalDate, schema:GraphSpec): SnapshotGraphWithSchema = {
-    loadWithSchemaAndPartition(dataPath, start, end, schema, PartitionStrategyType.None, 0)
-  }
-
-  final def loadWithSchemaAndPartition(dataPath: String, start: LocalDate, end: LocalDate, schema: GraphSpec, strategy: PartitionStrategyType.Value, runWidth: Int): SnapshotGraphWithSchema = {
-    var minDate: LocalDate = start
-    var maxDate: LocalDate = end
-
-    val (fullInterval: Interval, res: Resolution) = GraphLoader.loadGraphSpan(dataPath)
-    if (fullInterval.start.isAfter(start)) 
-      minDate = fullInterval.start
-    if (fullInterval.end.isBefore(end)) 
-      maxDate = fullInterval.end
-
-    if (minDate.isAfter(maxDate) || minDate.isEqual(maxDate))
-      throw new IllegalArgumentException("invalid date range")
-
-    var intvs: Seq[Interval] = Seq[Interval]()
-    var gps: ParSeq[Graph[InternalRow, InternalRow]] = ParSeq[Graph[InternalRow, InternalRow]]()
-    var xx:LocalDate = minDate
-
-    val conf: Configuration = new Configuration()
-    if (System.getenv("HADOOP_CONF_DIR") != "") {
-      conf.addResource(new Path(System.getenv("HADOOP_CONF_DIR") + "/core-site.xml"))
-    }
-    val fs: FileSystem = FileSystem.get(conf)
-
-    val fullSchema = GraphLoader.loadGraphDescription(dataPath)
-    //FIXME: throw exception if the schema passed in has fields
-    //that the full schema does not have!
-
-    val vertexAllFields = fullSchema.getVertexSchema
-    val edgeAllFields = fullSchema.getEdgeSchema
-    val (vertexFields: Array[Boolean], edgeFields: Array[Boolean]) = schema.getIndices(fullSchema)
-
-    val total = res.numBetween(minDate, maxDate)
-    while (xx.isBefore(maxDate)) {
-      var nodesPath = dataPath + "/nodes/nodes" + xx.toString() + ".txt"
-      var edgesPath = dataPath + "/edges/edges" + xx.toString() + ".txt"
-      var numNodeParts = MultifileLoad.estimateParts(nodesPath) 
-      var numEdgeParts = MultifileLoad.estimateParts(edgesPath) 
-      
-      //FIXME: comma is not a good separator
-      val users: RDD[(VertexId, InternalRow)] = ProgramContext.sc.textFile(dataPath + "/nodes/nodes" + xx.toString() + ".txt", numNodeParts).map(line => line.split(",")).flatMap { parts =>
-        if (parts.size >= 1 && parts.head != "") {
-          val id: VertexId = parts.head.toLong
-
-          //load based on the schema, which might skip some fields
-          //but names have to be the same
-          //id is not listed in the schema because it is required and assumed
-          val row = vertexFields.zipWithIndex.map { case (field,index) =>
-            if (field) TypeParser.parseValueByType(parts(index+1), vertexAllFields(index).dataType)
-          }
-          Some((id, new GenericInternalRow(row)))
-        } else None
-      }
-      
-      //FIXME: space is not a good delimiter
-      val edgePath = dataPath + "/edges/edges" + xx.toString() + ".txt"
-      val ept = new Path(edgePath)
-      val edges: EdgeRDD[InternalRow] = if (fs.exists(ept) && fs.getFileStatus(ept).getLen > 0) {
-        EdgeRDD.fromEdges(ProgramContext.sc.textFile(edgePath, numEdgeParts).map(line => line.split(" ")).flatMap { parts =>
-          if (parts.size >= 2 && parts.head != "") {
-            val vid1: VertexId = parts(0).toLong
-            val vid2: VertexId = parts(0).toLong
-            val row = edgeFields.zipWithIndex.map { case (field, index) =>
-              if (field) TypeParser.parseValueByType(parts(index+2), edgeAllFields(index).dataType)
-            }
-            val e: Edge[InternalRow] = Edge(vid1, vid2, new GenericInternalRow(row))
-            Some(e)
-          } else None
-        })
-      } else EdgeRDD.fromEdges[InternalRow,InternalRow](ProgramContext.sc.emptyRDD)
-      intvs = intvs :+ res.getInterval(xx)
-      gps = if (strategy == PartitionStrategyType.None)
-        gps :+ Graph(users, edges)
-      else
-        gps :+ Graph(users, edges).partitionBy(PartitionStrategies.makeStrategy(strategy, intvs.size + 1, total, runWidth), edges.partitions.size)
-      xx = intvs.last.end
-    }
-
-    val result = new SnapshotGraphWithSchema(intvs, gps)
-    result.setSchema(schema)
-    result
-  }
+  def emptyGraph(schema: GraphSpec):SnapshotGraphWithSchema = new SnapshotGraphWithSchema(Seq[Interval](), sqlContext.createDataFrame(ProgramContext.sc.emptyRDD[Row], StructType(schema.getVertexSchema)), sqlContext.createDataFrame(ProgramContext.sc.emptyRDD[Row], StructType(schema.getEdgeSchema)), ParSeq[Graph[Row,Row]]())
 }
