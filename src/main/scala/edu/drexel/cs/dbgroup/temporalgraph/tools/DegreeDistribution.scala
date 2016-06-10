1a742a3291104bee228d525c7ef35ade320001ee
shishir <sk3432@drexel.edu>
2016-06-10 14:19:19 -0400

    added degreeDistribution, still need to find vertices with 0 degree

---
+++ src/main/scala/edu/drexel/cs/dbgroup/temporalgraph/tools/DegreeDistribution.scala
@@ -0,0 +1,54 @@
+package edu.drexel.cs.dbgroup.temporalgraph.tools
+
+import java.io.FileWriter
+import java.time.LocalDate
+
+import edu.drexel.cs.dbgroup.temporalgraph.{Interval, TGraphNoSchema, ProgramContext}
+import edu.drexel.cs.dbgroup.temporalgraph.util.GraphLoader
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.graphx.VertexId
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkContext, SparkConf}
+import edu.drexel.cs.dbgroup.temporalgraph.tools.HistogramFromParquet.createDatesArrayByMonth
+import edu.drexel.cs.dbgroup.temporalgraph.tools.HistogramFromParquet.createDatesArrayByYear
+
+/**
+  * Created by shishir on 6/6/2016.
+  */
+object DegreeDistribution {
+  //note: this does not remove ALL logging
+  Logger.getLogger("org").setLevel(Level.OFF)
+  Logger.getLogger("akka").setLevel(Level.OFF)
+  var conf = new SparkConf().setAppName("TemporalGraph Project").setSparkHome(System.getenv("SPARK_HOME"))
+  val sc = new SparkContext(conf)
+  ProgramContext.setContext(sc)
+  val sqlContext = new SQLContext(sc)
+
+
+  def main(args: Array[String]): Unit ={
+    createDegreeDistribution("./dblp", "test.txt")
+//    createDegreeDistribution("hdfs://master:9000/data/arxiv/", "DatasetStatistics/Degrees/Data/arxiv.txt")
+//    createDegreeDistribution("hdfs://master:9000/data/nGrams/", "DatasetStatistics/Degrees/Data/nGrams.txt")
+  }
+
+  def createDegreeDistribution(source:String, directoryName:String): Unit ={
+    val SGP = GraphLoader.loadDataParquet(source)
+    val degrees = SGP.degree()
+
+
+    //test data
+//    val degrees = sc.parallelize((1L to 5L).map(x => (x, (Interval(LocalDate.parse("2015-01-01"), LocalDate.parse("2016-01-01")), Math.abs(x-4)))))
+
+    //degree distribution by each year
+    //val textFile = (1936 to 2015).map(x => degrees.filter(_._2._1.intersects(Interval(LocalDate.parse(x.toString + "-01-01")
+    //, LocalDate.parse((x+1).toString + "-01-01")))).map(x => (x._2._2, 1)).reduceByKey(_+_).map(_.toString()).saveAsTextFile(x.toString))
+
+    val degreeDistribution = degrees.map(x => (x._2._2, 1)).reduceByKey(_+_).sortByKey().collect()
+    val fw = new FileWriter(directoryName, true)
+    for ( x <- degreeDistribution ) {
+      fw.write(x._1.toString() + " " + x._2.toString + "\n")
+    }
+    fw.close()
+  }
+}
