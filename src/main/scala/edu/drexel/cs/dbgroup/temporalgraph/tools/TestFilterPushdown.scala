f60f6a5da521581856f1f92f3f3440e38ae42ce3
shishir <sk3432@drexel.edu>
2016-09-08 16:20:23 -0400

    updated tools for spark 2.0

--- src/main/scala/edu/drexel/cs/dbgroup/temporalgraph/tools/TestFilterPushdown.scala
+++ src/main/scala/edu/drexel/cs/dbgroup/temporalgraph/tools/TestFilterPushdown.scala
@@ -15,16 +15,45 @@ object TestFilterPushdown {
   Logger.getLogger("org").setLevel(Level.OFF)
   Logger.getLogger("akka").setLevel(Level.OFF)
   var conf = new SparkConf().setAppName("TemporalGraph Project").setSparkHome(System.getenv("SPARK_HOME"))
+
   conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
   conf.set("spark.network.timeout", "240")
-  conf.set("spark.shuffle.spill.numElementsForceSpillThreshold", "500000")
+  //conf.set("spark.shuffle.spill.numElementsForceSpillThreshold", "500000")
   val sc = new SparkContext(conf)
   ProgramContext.setContext(sc)
-  val sqlContext = new SQLContext(sc)
+  val sqlContext = ProgramContext.getSession
+  sqlContext.conf.set("spark.sql.files.maxPartitionBytes", "16777216")
   import sqlContext.implicits._
 
   def main(args: Array[String]): Unit ={
-    run("./dblp/edges.parquet", "1952-01-01", "2013-01-01")
+    run("C:/Users/shishir/temporaldata/dblp/edges.parquet", "1952-01-01", "2013-01-01")
+    runVid("hdfs://master:9000/data/twitter/edges.parquet", 10000, 1000000)
+  }
+
+  def runVid(source:String, dataWithFewCounts:Long, dateWithManyCounts:Long): Unit ={
+    val data = sqlContext.read.parquet(source)
+    var start, end, timeTaken: Long = 0
+    data.registerTempTable("tempTable")
+    //executing test predicate to make sure the tempTable is registered
+    var output = sqlContext.sql("Select * from tempTable")
+    println(output.count())
+
+    start = System.currentTimeMillis()
+    var sqlQuery = "Select vid1 from tempTable where vid1<" +  dataWithFewCounts
+    output = sqlContext.sql(sqlQuery)
+    println(output.count())
+    end = System.currentTimeMillis()
+    timeTaken =  end - start
+    println("Time taken for one Predicate with few counts " + timeTaken/1000 + "s")
+
+    //to test if number of counts affect performance keeping number of predicate constant, it looks like it doesnt
+    start = System.currentTimeMillis()
+    sqlQuery = "Select vid1 from tempTable where vid1<" +  dateWithManyCounts
+    output = sqlContext.sql(sqlQuery)
+    println(output.count())
+    end = System.currentTimeMillis()
+    timeTaken =  end - start
+    println("Time taken for one Predicate with large counts " + timeTaken/1000 + "s")
   }
 
   def run(source:String, dataWithFewCounts:String, dateWithManyCounts:String): Unit ={
