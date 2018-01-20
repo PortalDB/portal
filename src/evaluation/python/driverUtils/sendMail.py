aeaaf39f01f63a8e2f22e2e26d8b1230ef2fe011
Matthew T. Gilbride <mtg5014@gmail.com>
2018-01-20 18:14:00 -0500

    1.0 Release

--- src/evaluation/python/driverUtils/sendMail.py
+++
@@ -1,8 +0,0 @@
-import smtplib
-
-def sendMail(email, msg):
-    server = smtplib.SMTP_SSL('smtp.googlemail.com', 465)
-    #server.starttls()
-    server.login("dbgroupdrexel@gmail.com", "ilovedbilovedb")
-    server.sendmail("dbgroupdrexel@gmail.com", email, msg)
-    server.quit()
